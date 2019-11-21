c2ede9e14925c92ca62f90860e4812fbbbb70bff
ChudanKesa <e.lequere77@gmail.com>
2019-11-21 09:40:09 +0100

    Init

---
+++ LeBaluchonTests/LeBaluchonTests.swift
@@ -0,0 +1,36 @@
+//
+//  LeBaluchonTests.swift
+//  LeBaluchonTests
+//
+//  Created by Erwan Le Querré on 21/11/2019.
+//  Copyright © 2019 Erwan Le Querré. All rights reserved.
+//
+
+import XCTest
+@testable import LeBaluchon
+
+class LeBaluchonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
